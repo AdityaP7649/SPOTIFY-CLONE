684972e1073116e6a1f5f56b648cceb319645c51
Aditya Patil <adityapatil7649@gmail.com>
2024-01-31 23:35:59 +0530

    Created the Song Cards

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- 
-async function getsongs(){
+
+async function getsongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text();
     let div = document.createElement("div")
@@ -15,23 +15,33 @@ async function getsongs(){
     return songs;
 }
 
-async function main(){
+async function main() {
     //Get the list of all the songs
     let songs = await getsongs()
     console.log(songs)
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20" , " ")}</li.`;
+        songUL.innerHTML = songUL.innerHTML +
+            `<li>
+            <img src="/assets/music.svg" alt="">
+                <div class="info">
+                    <div style="padding-top: 3px;">${song.replaceAll("%20", " ")}</div>
+                </div>
+                <div class="playnow">
+                    <span style="padding-left: 20px;">Play Now</span>
+                    <img style="padding-left: 5px;" src="assets/play.svg" alt=""> 
+                </div>
+         </li>`;
     }
 
     // Play the first song
     var audio = new Audio(songs[0]);
     //audio.play();
 
-    audio.addEventListener("loadeddata" , () => {
+    audio.addEventListener("loadeddata", () => {
         let duration = audio.duration;
-        console.log(audio.duration , audio.currentSrc , audio.currentTime)
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
     })
 }
 
